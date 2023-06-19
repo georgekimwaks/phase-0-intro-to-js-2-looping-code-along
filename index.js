f897991c23c9137fef7589b46c980310b108a34d
george kimwaki <kimwaksgeorge1@gmail.com>
2023-06-20 02:13:22 +0300

    unfinished

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Dear ${names[i]}, congratulations on ${eventName}! Best wishes.`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
